56ad94f51614a761a112af9acf770c780eef3a02
Kamil Lenkiewicz <k.lenkiewicz@stud.elka.pw.edu.pl>
2018-01-19 16:09:37 +0100

    add random functions generator

--- data_generator.py
+++ data_generator.py
@@ -7,7 +7,6 @@ import json
 from location import Location2D
 from resource import Resource, ResourceRequirement
 
-
 def main():
     """Random resource generator entry point."""
     parser = argparse.ArgumentParser(description='A simple program to generate data for factory location problem.')
@@ -28,16 +27,15 @@ def main():
     for resource_requirement in resources:
         resource = resource_requirement.resource
         resource_location = resource.location
-        function_string = function_to_string(resource)
         print("{} {} {} {}".format(resource_location.position_x,
                                    resource_location.position_y,
                                    resource_requirement.required_units,
-                                   function_string))
+                                   resource.unit_transport_cost_function))
         # prepare object to save
         json_res = dict()
         json_res["Position"] = [resource_location.position_x, resource_location.position_y]
         json_res["Required_units"] = resource_requirement.required_units
-        json_res["Transport_cost_func"] = function_string
+        json_res["Transport_cost_func"] = resource.unit_transport_cost_function
         json_main_obj["Resources"].append(json_res)
 
     # save to file
@@ -53,26 +51,35 @@ def generate_random_resources(args):
         resource_position_x = random.uniform(args.min_pos, args.max_pos)
         resource_position_y = random.uniform(args.min_pos, args.max_pos)
         required_units = random.uniform(args.min_req_units, args.max_req_units)
-        transport_cost_function = get_random_function()
+        transport_cost_function = get_random_function_string()
         resource = Resource(Location2D(resource_position_x, resource_position_y), transport_cost_function)
         resource_requirement = ResourceRequirement(resource, required_units)
         resources.append(resource_requirement)
     return resources
 
 
-def get_random_function():
+def get_random_function_string():
     """Returns random function."""
-    # TODO generate random non-linear functions
-    def f(d): return d
-    return f
+    function_type = random.randint(0, 2)
+    if function_type == 0:
+        # linear function with random coefficients
+        a = random.randint(0, 10)
+        b = random.randint(0, 10)
+        return "def f(x): return x*{}+{}".format(a, b)
+    elif function_type == 1:
+        # polynomial function
+        power = random.randint(0, 10)
+        return create_polynomial_function_string(power)
+    else:
+        # root function
+        a = random.randint(0, 10)
+        return "def f(x): return {}*x**0.5".format(a)
 
 
-def function_to_string(resource):
-    """Changes function to string."""
-    function_source_lines = inspect.getsourcelines(resource.unit_transport_cost_function)
-    function_string = "".join((function_source_lines[0]))
-    # remove leading and trailing whitespaces to prevent empty lines
-    return function_string.strip()
+# power has to be an integer number
+def create_polynomial_function_string(power):
+    coefficients_string = "    coefficients = [" + str.join(', ', [str(random.randint(0, 10)) for _ in range(power)]) + "]\n"
+    return "def f(x):\n" + coefficients_string + "    return sum([coefficients[i]*(x**(i/3.0)) for i in range({})])".format(power)
 
 
 if __name__ == "__main__":
